de0c54e461444017900050521e2f7e72d1e9707f
OmerCevruk <omer.cevrukk@gmail.com>
2023-12-22 21:59:03 +0300

    some minor changes

--- thesis_project/settings.py
+++ thesis_project/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'thesis_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': 'pataos',
+        'PORT': '',
+        'HOST': 'OmerCevruk',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+        },
+
     }
 }
 

fc5ca377c997bfb11a250c603c4d4edf14428b36
OmerCevruk <omer.cevrukk@gmail.com>
2023-12-22 22:46:51 +0300

    ikinci commit

--- thesis/admin.py
+++ thesis/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import University  # Import your model
+@admin.register(University)
+class UniversityAdmin(admin.ModelAdmin):
+    list_display = ('university_id', 'name', 'establishment_year')
--- thesis/models.py
+++ thesis/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class University(models.Model):
+    university_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)  # Fix the typo in max_length
+    establishment_year = models.SmallIntegerField()
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(establishment_year__gt=0),
+                name='establishment_year_positive_check'
+            )
+        ]

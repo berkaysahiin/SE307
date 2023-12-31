7ee9e4a2919b1137aafa1b35f71ceba6ab8cdc04
Berkay Sahin <berkaysahindev@gmail.com>
2024-01-01 01:39:40 +0300

    Update models.py

--- thesis/models.py
+++ thesis/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Institute(models.Model):
-    institute_id = models.IntegerField(db_column='INSTITUTE_ID', primary_key=True)  # Field name made lowercase.
-    name = models.CharField(db_column='NAME', max_length=255, db_collation='Turkish_CI_AS')  # Field name made lowercase.
+    institute_id = models.IntegerField(db_column='INSTITUTE_ID', primary_key=True) 
+    name = models.CharField(db_column='NAME', max_length=255, db_collation='Turkish_CI_AS')  
     university = models.ForeignKey('University', on_delete=models.CASCADE, db_column='UNIVERSITY_ID')
 
     class Meta:
@@ -14,8 +14,8 @@ class Institute(models.Model):
 
 
 class Language(models.Model):
-    language_id = models.IntegerField(db_column='LANGUAGE_ID', primary_key=True)  # Field name made lowercase.
-    language_name = models.CharField(db_column='LANGUAGE_NAME', max_length=255, db_collation='Turkish_CI_AS')  # Field name made lowercase.
+    language_id = models.IntegerField(db_column='LANGUAGE_ID', primary_key=True) 
+    language_name = models.CharField(db_column='LANGUAGE_NAME', max_length=255, db_collation='Turkish_CI_AS')  
 
     class Meta:
         managed = False
@@ -26,10 +26,10 @@ class Language(models.Model):
 
 
 class Person(models.Model):
-    person_id = models.IntegerField(db_column='PERSON_ID', primary_key=True)  # Field name made lowercase.
-    name = models.CharField(db_column='NAME', max_length=255, db_collation='Turkish_CI_AS')  # Field name made lowercase.
-    surname = models.CharField(db_column='SURNAME', max_length=255, db_collation='Turkish_CI_AS')  # Field name made lowercase.
-    birth_date = models.DateField(db_column='BIRTH_DATE')  # Field name made lowercase.
+    person_id = models.IntegerField(db_column='PERSON_ID', primary_key=True)  
+    name = models.CharField(db_column='NAME', max_length=255, db_collation='Turkish_CI_AS')  
+    surname = models.CharField(db_column='SURNAME', max_length=255, db_collation='Turkish_CI_AS')  
+    birth_date = models.DateField(db_column='BIRTH_DATE')  
 
     class Meta:
         managed = False
@@ -40,8 +40,8 @@ class Person(models.Model):
 
 
 class Subject(models.Model):
-    subject_id = models.IntegerField(db_column='SUBJECT_ID', primary_key=True)  # Field name made lowercase.
-    topic = models.CharField(db_column='TOPIC', max_length=1000, db_collation='Turkish_CI_AS')  # Field name made lowercase.
+    subject_id = models.IntegerField(db_column='SUBJECT_ID', primary_key=True)  
+    topic = models.CharField(db_column='TOPIC', max_length=1000, db_collation='Turkish_CI_AS')  
 
     class Meta:
         managed = False
@@ -52,19 +52,19 @@ class Subject(models.Model):
 
 
 class Thesis(models.Model):
-    thesis_no = models.DecimalField(db_column='THESIS_NO', primary_key=True, max_digits=7, decimal_places=0)  # Field name made lowercase.
-    title = models.CharField(db_column='TITLE', max_length=500, db_collation='Turkish_CI_AS')  # Field name made lowercase.
-    abstract = models.CharField(db_column='ABSTRACT', max_length=4000, db_collation='Turkish_CI_AS')  # Field name made lowercase.
-    author = models.ForeignKey(Person, on_delete=models.PROTECT, db_column='AUTHOR_ID')  # Field name made lowercase.
-    year = models.SmallIntegerField(db_column='YEAR')  # Field name made lowercase.
-    type = models.ForeignKey('Type', models.PROTECT, db_column='TYPE_ID')  # Field name made lowercase.
-    university = models.ForeignKey('University', models.PROTECT, db_column='UNIVERSITY_ID')  # Field name made lowercase.
-    institute = models.ForeignKey(Institute, on_delete=models.PROTECT, db_column='INSTITUTE_ID')  # Field name made lowercase.
-    number_of_pages = models.IntegerField(db_column='NUMBER_OF_PAGES')  # Field name made lowercase.
-    language = models.ForeignKey(Language, on_delete=models.PROTECT, db_column='LANGUAGE_ID')  # Field name made lowercase.
+    thesis_no = models.DecimalField(db_column='THESIS_NO', primary_key=True, max_digits=7, decimal_places=0)  
+    title = models.CharField(db_column='TITLE', max_length=500, db_collation='Turkish_CI_AS')  
+    abstract = models.CharField(db_column='ABSTRACT', max_length=4000, db_collation='Turkish_CI_AS')  
+    author = models.ForeignKey(Person, on_delete=models.PROTECT, db_column='AUTHOR_ID')  
+    year = models.SmallIntegerField(db_column='YEAR')  
+    type = models.ForeignKey('Type', models.PROTECT, db_column='TYPE_ID')  
+    university = models.ForeignKey('University', models.PROTECT, db_column='UNIVERSITY_ID')  
+    institute = models.ForeignKey(Institute, on_delete=models.PROTECT, db_column='INSTITUTE_ID')  
+    number_of_pages = models.IntegerField(db_column='NUMBER_OF_PAGES') 
+    language = models.ForeignKey(Language, on_delete=models.PROTECT, db_column='LANGUAGE_ID') 
     superviser = models.ForeignKey(Person, on_delete=models.PROTECT, db_column='SUPERVISER', related_name='thesis_superviser_set')  # Field name made lowercase.
     cosuperviser = models.ForeignKey(Person, on_delete=models.PROTECT, db_column='COSUPERVISER', related_name='thesis_cosuperviser_set', blank=True, null=True)  # Field name made lowercase.
-    submission_date = models.DateField(db_column='SUBMISSION_DATE')  # Field name made lowercase.
+    submission_date = models.DateField(db_column='SUBMISSION_DATE')  
 
     @property
     def keywords(self):
@@ -83,7 +83,7 @@ class Thesis(models.Model):
 
 class ThesisKeyword(models.Model):
     thesis_no = models.OneToOneField(Thesis, on_delete=models.CASCADE, db_column='THESIS_NO', primary_key=True)
-    keyword = models.CharField(db_column='KEYWORD', max_length=255, db_collation='Turkish_CI_AS')  # Field name made lowercase.
+    keyword = models.CharField(db_column='KEYWORD', max_length=255, db_collation='Turkish_CI_AS')  
 
     class Meta:
         managed = False
@@ -106,8 +106,8 @@ class ThesisSubject(models.Model):
 
 
 class Type(models.Model):
-    type_id = models.IntegerField(db_column='TYPE_ID', primary_key=True)  # Field name made lowercase.
-    type_name = models.CharField(db_column='TYPE_NAME', max_length=255, db_collation='Turkish_CI_AS')  # Field name made lowercase.
+    type_id = models.IntegerField(db_column='TYPE_ID', primary_key=True)  
+    type_name = models.CharField(db_column='TYPE_NAME', max_length=255, db_collation='Turkish_CI_AS')  
 
     class Meta:
         managed = False
@@ -118,9 +118,9 @@ class Type(models.Model):
 
 
 class University(models.Model):
-    university_id = models.IntegerField(db_column='UNIVERSITY_ID', primary_key=True)  # Field name made lowercase.
-    name = models.CharField(db_column='NAME', max_length=255, db_collation='Turkish_CI_AS')  # Field name made lowercase.
-    establishment_year = models.SmallIntegerField(db_column='ESTABLISHMENT_YEAR')  # Field name made lowercase.
+    university_id = models.IntegerField(db_column='UNIVERSITY_ID', primary_key=True)  
+    name = models.CharField(db_column='NAME', max_length=255, db_collation='Turkish_CI_AS')  
+    establishment_year = models.SmallIntegerField(db_column='ESTABLISHMENT_YEAR')  
 
     @property
     def institutes(self):
